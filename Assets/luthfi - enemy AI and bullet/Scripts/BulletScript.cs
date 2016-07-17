128e0a372e4a3efcf40fc4b6bf7022407d958d74
Muhammad Luthfi <luthfi.muhammad825@gmail.com>
2016-07-17 21:08:01 +0700

    add empty animator and audio source, fix build setting

---
+++ Assets/luthfi - enemy AI and bullet/Scripts/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+	Animator anim;
+	AudioSource au;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		au = GetComponent<AudioSource> ();
+
+		//bunyiin bullet (kalo perlu)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D coll) {
+		/*if gameobject == player
+			nyalain animasi hit
+			destroy gameobject
+		*/
+	}
+}
